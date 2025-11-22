fd7d7ed627b85fa7dd163618dc297c6bac1c4bdb
udaygit57 <udaygupta3635@gmail.com>
2025-11-23 02:14:57 +0530

    Update data.js

--- data.js
+++ data.js
@@ -3,26 +3,27 @@ let songs=[
     title: "All Black",
     artist: "Raftar",
     Path: "Music/All Black.mp3",
-    cover: "image/AllBlack.jpg"
+    cover: "AllBlack.jpg"
 },
 {
     title: "Lemonade",
     artist: "Diljit Dosanjh",
     Path: "Music/Lemonade.mp3",
-    cover: "image/lamonade.jpg"
+    cover: "lamonade.jpg"
 },
 {
     title: "Love Dose",
     artist: "Yo Yo Honey Singh",
     Path: "Music/Love Dose.mp3",
-    cover: "image/LoveDose.jpg"
+    cover: "LoveDose.jpg"
 },
 {
     title: "Agar Tum Saath Ho",
     artist: "Alka Yagnik, Arijit Singh",
     Path: "Music/Agar Tum Saath Ho (From _Tamasha_).mp3",
-    cover: "image/AgarTumSaathHo.jpg"
+    cover: "AgarTumSaathHo.jpg"
 },
+
 
 
 ]
